6dcc8a3c08026d954e76aa052cd57ee4464cf543
jussell2017 <jussell1983@gmail.com>
2021-04-27 21:06:42 -0400

    Student view working

--- SchoolManagementSystem/Models/StudentVM.cs
+++ SchoolManagementSystem/Models/StudentVM.cs
@@ -11,17 +11,6 @@ namespace SchoolManagementSystem.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string MiddleName { get; set; }
-        public DateTime DOB { get; set; }
-        public char HeightFeet { get; set; }
-        public char HeightInches { get; set; }
-        public string Address { get; set; }
-        public string StudendEmail { get; set; }
-        public string StudentPhoneNumber { get; set; }
-        public char BirthEntryNum { get; set; }
-        public char StudentId { get; set; }
-        public char StudentGrade { get; set; }
-        public char StudentClass { get; set; }
-        public string StudentHouse { get; set; }
-        public string StudentAdditionalInfo { get; set; }
+        
     }
 }
