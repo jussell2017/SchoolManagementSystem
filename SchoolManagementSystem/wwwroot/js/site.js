af9a4766ff4fb7ccc5ed4448b6a515eef5bc56e4
EldeenJohnson <andretdh@gmail.com>
2021-05-04 19:31:37 -0500

    Form adjustments - Combo box added.

--- SchoolManagementSystem/wwwroot/js/site.js
+++ SchoolManagementSystem/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//This edits the look of the table layout
+$(document).ready(function () {
+    $('#tblData').DataTable();
+});
